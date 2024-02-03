25c478c2d0bbdf8e8694b21ebba1399f89c2021d
yusei <yusei21koga@gmail.com>
2024-02-04 08:08:05 +0900

    taxonomy-voice_category.php 追加

---
+++ OriginalTheme/taxonomy-voice_category.php
@@ -0,0 +1,69 @@
+<?php get_header(); ?>
+
+<section class="archive-voice layout-sub-contents ornament">
+  <div class="inner">
+
+    <!-- タブメニュー -->
+    <div class="archive-voice__tab tab">
+      <?php
+      $current_term = get_queried_object();
+      $is_term_page = $current_term instanceof WP_Term && $current_term->taxonomy === 'voice_category';
+      ?>
+      <!-- ALL Tab -->
+      <a href="<?php echo get_post_type_archive_link('voice'); ?>"
+        class="tab__item <?php if (!$is_term_page)  echo 'is-active'; ?>">ALL</a>
+      <!-- Terms Tab -->
+      <?php
+      $terms = get_terms([
+        'taxonomy' => 'voice_category',
+        'hide_empty' => true,
+      ]);
+      foreach ($terms as $term) {
+        $is_active = $is_term_page && ($current_term->term_id === $term->term_id);
+        echo '<a href="' . esc_url(get_term_link($term)) . '" class="tab__item ' . ($is_active ? 'is-active' : '') . '">' . esc_html($term->name) . '</a>';
+      }
+      ?>
+    </div>
+
+    <!-- カードリスト -->
+    <?php if(have_posts()) : ?>
+    <div class="archive-voice__items voice-cards">
+      <?php while(have_posts()) : the_post(); ?>
+      <?php
+      // ACFから値を取得
+      $age = get_field('age');
+      $type = get_field('type');
+      $text = get_field('text');
+      // カスタムタクソノミーを取得
+      $terms = get_the_terms( get_the_ID(), 'voice_category');
+      $term_name = !empty($terms) ? esc_html($terms[0]->name) : '未分類';
+      //　画像を取得
+      $image_url = has_post_thumbnail() ? get_the_post_thumbnail_url() : get_theme_file_uri('/assets/images/common/no-image.webp');
+      ?>
+      <article class="voice-cards__item voice-card">
+        <div class="voice-card__head">
+          <div class="voice-card__content">
+            <div class="voice-card__meta">
+              <span class="voice-card__tag"><?php echo esc_html($age); ?>代(<?php echo esc_html($type); ?>)</span>
+              <span class="voice-card__category"><?php echo $term_name; ?></span>
+            </div>
+            <h3 class="voice-card__title"><?php the_title(); ?></h3>
+          </div>
+          <figure class="voice-card__img">
+            <img src="<?php echo esc_url($image_url); ?>" alt="">
+          </figure>
+        </div>
+        <div class="voice-card__body">
+          <p class="voice-card__text"><?php echo esc_html($text); ?></p>
+        </div>
+      </article>
+      <?php endwhile; ?>
+    </div>
+    <?php endif; ?>
+
+    <!-- ページネーション -->
+    <?php get_template_part('template-parts/pagenavi') ?>
+  </div>
+</section>
+
+<?php get_footer(); ?>
