c609a48f2bcb4bde7f333be39450e187ab405e21
yusei <yusei21koga@gmail.com>
2024-02-03 14:51:57 +0900

    page-sitemap.php 追加

---
+++ OriginalTheme/page-sitemap.php
@@ -0,0 +1,55 @@
+<?php get_header(); ?>
+
+<section class="sitemap layout-sub-contents ornament">
+  <div class="inner">
+    <!-- サイトマップリンク -->
+    <div class="sitemap__container">
+      <div class="sitemap__wrap">
+        <ul class="sitemap__items">
+          <li class="sitemap__item">
+            <a href="./campaign.html">キャンペーン</a>
+            <ul class="sitemap__sub-items">
+              <li class="sitemap__sub-item"><a href="./campaign.html">ライセンス取得</a></li>
+              <li class="sitemap__sub-item"><a href="./campaign.html">貸切体験ダイビング</a></li>
+              <li class="sitemap__sub-item"><a href="./campaign.html">ナイトダイビング</a></li>
+            </ul>
+          </li>
+          <li class="sitemap__item"><a href="./about.html">私たちについて</a></li>
+        </ul>
+        <ul class="sitemap__items">
+          <li class="sitemap__item">
+            <a href="./information.html">ダイビング情報</a>
+            <ul class="sitemap__sub-items">
+              <li class="sitemap__sub-item"><a href="./information.html#tab01">ライセンス講習</a></li>
+              <li class="sitemap__sub-item"><a href="./information.html#tab03">体験ダイビング</a></li>
+              <li class="sitemap__sub-item"><a href="./information.html#tab02">ファンダイビング</a></li>
+            </ul>
+          </li>
+          <li class="sitemap__item"><a href="./blog.html">ブログ</a></li>
+        </ul>
+      </div>
+      <div class="sitemap__wrap">
+        <ul class="sitemap__items">
+          <li class="sitemap__item"><a href="./voice.html">お客様の声</a></li>
+          <li class="sitemap__item">
+            <a href="./price.html">料金一覧</a>
+            <ul class="sitemap__sub-items">
+              <li class="sitemap__sub-item"><a href="./price.html#priceLicence">ライセンス講習</a></li>
+              <li class="sitemap__sub-item"><a href="./price.html#priceExperience">体験ダイビング</a></li>
+              <li class="sitemap__sub-item"><a href="./price.html#priceFun">ファンダイビング</a></li>
+            </ul>
+          </li>
+        </ul>
+        <ul class="sitemap__items">
+          <li class="sitemap__item"><a href="./faq.html">よくある質問</a></li>
+          <li class="sitemap__item"><a href="./privacy.html">プライバシー<br class="u-mobile">ポリシー</a></li>
+          <li class="sitemap__item"><a href="./terms.html">利用規約</a></li>
+          <li class="sitemap__item"><a href="./contact.html">お問い合わせ</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</section>
+</main>
+
+<?php get_footer(); ?>
