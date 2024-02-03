e63b9caca9f28accfa9608576d94e8f402256f00
yusei <yusei21koga@gmail.com>
2024-02-03 16:35:33 +0900

    404.php 追加

---
+++ OriginalTheme/404.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+
+<div class="not-found">
+  <!-- パンくず -->
+  <div class="breadcrumb breadcrumb--white layout-breadcrumb">
+    <div class="inner">
+      <span><a href="#">TOP</a></span>
+      <span>404</span>
+    </div>
+  </div>
+  <!-- 404 -->
+  <div class="not-found__container">
+    <p class="not-found__title">404</p>
+    <p class="not-found__text">
+      申し訳ありません。<br>
+      お探しのページが見つかりません。
+    </p>
+    <div class="not-found__button">
+      <a href="./index.html" class="button button--white">Page Top<span></span></a>
+    </div>
+  </div>
+</div>
+
+<?php get_footer(); ?>
