6ed6a37b4ac7293bcd679ca9dcefff6b87b6a54f
yusei <yusei21koga@gmail.com>
2024-02-03 10:32:04 +0900

    パンくずリスト 共通パーツ 追加

---
+++ OriginalTheme/template-parts/breadcrumb.php
@@ -0,0 +1,9 @@
+<?php if(function_exists('bcn_display')) : ?>
+<div class="breadcrumb layout-breadcrumb">
+  <div class="inner">
+    <div class="breadcrumb__container">
+      <?php bcn_display(); ?>
+    </div>
+  </div>
+</div>
+<?php endif; ?>
