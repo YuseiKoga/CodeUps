f68b528eb864a5b4161cc66e4c92fed6be3f088a
yusei <yusei21koga@gmail.com>
2024-02-13 12:26:54 +0900

    sidebar 条件分岐追加

--- OriginalTheme/sidebar.php
+++ OriginalTheme/sidebar.php
@@ -1,30 +1,29 @@
 <aside class="column__side sidebar">
 
   <!-- 人気記事 -->
-  <section class="sidebar__popular popular layout-sidebar-section">
-    <h2 class="sidebar-title">人気記事</h2>
-
-    <!-- カードリスト -->
-    <?php
+  <?php
     $popular_posts_args = array(
       'posts_per_page' => 3, // 表示する記事の数
       'meta_key' => 'post_views_count', // 閲覧数を格納するカスタムフィールド
       'orderby' => 'meta_value_num', // 閲覧数に基づいて並べ替え
       'order' => 'DESC' // 降順で表示
-      );
+    );
 
     $popular_posts_query = new WP_Query($popular_posts_args);
 
     if ($popular_posts_query->have_posts()) :
-    ?>
+  ?>
+  <section class="sidebar__popular popular layout-sidebar-section">
+    <h2 class="sidebar-title">人気記事</h2>
+
+    <!-- カードリスト -->
     <div class="popular__items">
       <?php
-      // ループ START
-      while ($popular_posts_query->have_posts()) :
-        $popular_posts_query->the_post();
+        // ループ START
+        while ($popular_posts_query->have_posts()) : $popular_posts_query->the_post();
 
-      // 画像URLを取得、存在しない場合はデフォルト画像を設定
-      $image_url = has_post_thumbnail() ? get_the_post_thumbnail_url() : get_theme_file_uri('/assets/images/common/no-image.webp');
+        // 画像URLを取得、存在しない場合はデフォルト画像を設定
+        $image_url = has_post_thumbnail() ? get_the_post_thumbnail_url() : get_theme_file_uri('/assets/images/common/no-image.webp');
       ?>
 
       <article class="popular__item popular-card">
@@ -45,74 +44,76 @@
       </article>
       <?php endwhile; // ループ END ?>
     </div>
-    <?php endif; wp_reset_postdata(); ?>
   </section>
+  <?php endif; wp_reset_postdata(); ?>
 
 
   <!-- 口コミ -->
-  <section class="sidebar__comment comment layout-sidebar-section">
-    <h2 class="sidebar-title">口コミ</h2>
-
-    <?php
+  <?php
     $args = array(
       'post_type' => 'voice',
       'posts_per_page' => 1
     );
-    $voice_query = new WP_Query($args);
 
-    if ($voice_query->have_posts()) :
+    $voice_query = new WP_Query($args);
+  ?>
+
+  <?php if ($voice_query->have_posts()) : ?>
+  <section class="sidebar__comment comment layout-sidebar-section">
+    <h2 class="sidebar-title">口コミ</h2>
+
+    <?php
+    // ループ開始
       while ($voice_query->have_posts()) : $voice_query->the_post();
+
+      // カスタムフィールドがから値を取得
       $age = get_field('age');
       $type = get_field('type');
-    ?>
 
+      // 画像URLを取得、存在しない場合はデフォルト画像を設定
+      $image_url = has_post_thumbnail() ? get_the_post_thumbnail_url() : get_theme_file_uri('/assets/images/common/no-image.webp');
+    ?>
     <article class="comment__item comment-card">
       <figure class="comment-card__img">
-        <?php if (has_post_thumbnail()) : ?>
-        <?php the_post_thumbnail(); ?>
-        <?php else : ?>
-        <img src='<?php echo esc_url(get_theme_file_uri('/assets/images/common/no-image.webp')); ?>' alt=''>
-        <?php endif; ?>
+        <img src='<?php echo esc_url($image_url); ?>' alt=''>
       </figure>
       <div class="comment-card__body">
+        <?php if (!empty($age) && !empty($type)) : ?>
         <span class="comment-card__tag"><?php echo esc_html($age); ?>代(<?php echo esc_html($type); ?>)</span>
+        <?php endif; ?>
         <h3 class="comment-card__title"><?php the_title(); ?></h3>
         <div class="comment-card__button">
           <a href="<?php echo esc_url(home_url('/voice')); ?>" class="button">View more<span></span></a>
         </div>
       </div>
     </article>
-    <?php endwhile; endif; wp_reset_postdata(); ?>
+    <?php endwhile; ?>
   </section>
+  <?php endif; wp_reset_postdata(); ?>
 
 
   <!-- キャンペーン -->
+  <?php
+  // サブループ情報取得
+  $args = array(
+    'post_type' => 'campaign',
+    'posts_per_page' => 2
+  );
+  $campaign_query = new WP_Query($args);
+  ?>
+
+  <?php if ($campaign_query->have_posts()) : ?>
   <section class="sidebar__pr pr layout-sidebar-section">
     <h2 class="sidebar-title">キャンペーン</h2>
 
-    <?php
-    // サブループ情報取得
-    $args = array(
-      'post_type' => 'campaign',
-      'posts_per_page' => 2
-    );
-    $campaign_query = new WP_Query($args);
-    ?>
-
-    <?php if ($campaign_query->have_posts()) : ?>
     <div class="pr__items campaign-cards campaign-cards--side">
-
       <?php
       // ループ開始
       while ($campaign_query->have_posts()) : $campaign_query->the_post();
 
       // ACFから情報を取得
-      $regular_price = get_field('regular_price');
-      $special_price = get_field('special_price');
-
-      // 価格を3桁区切りにフォーマット
-      $formatted_regular_price = number_format($regular_price);
-      $formatted_special_price = number_format($special_price);
+      $regular_price = number_format(get_field('regular_price'));
+      $special_price = number_format(get_field('special_price'));
 
       // 画像URLを取得、存在しない場合はデフォルト画像を設定
       $image_url = has_post_thumbnail() ? get_the_post_thumbnail_url() : get_theme_file_uri('/assets/images/common/no-image.webp');
@@ -126,22 +127,25 @@
           <h3 class="campaign-card__title campaign-card__title--side"><?php the_title(); ?></h3>
           <p class="campaign-card__text campaign-card__text--side">全部コミコミ(お一人様)</p>
           <div class="campaign-card__price">
+            <?php if (!empty($regular_price)) : ?>
             <p class="campaign-card__before-price campaign-card__before-price--side">
-              ¥<?php echo esc_html($formatted_regular_price); ?></p>
+              ¥<?php echo esc_html($regular_price); ?></p>
+            <?php endif; ?>
+            <?php if (!empty($special_price)) : ?>
             <p class="campaign-card__after-price campaign-card__after-price--side">
-              ¥<?php echo esc_html($formatted_special_price); ?></p>
+              ¥<?php echo esc_html($special_price); ?></p>
+            <?php endif; ?>
           </div>
         </div>
       </article>
-
       <?php endwhile; // ループここまで ?>
     </div>
-    <?php endif; wp_reset_postdata(); ?>
 
     <div class="pr__button">
       <a href="<?php echo esc_url(home_url('/campaign')); ?>" class="button">View more<span></span></a>
     </div>
   </section>
+  <?php endif; wp_reset_postdata(); ?>
 
 
   <!-- アーカイブ -->
@@ -187,4 +191,5 @@
     echo '</ul>';
     ?>
   </section>
+
 </aside>
